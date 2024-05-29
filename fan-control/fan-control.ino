d83c330aa20680d877c66aced20de92a939eff9e
Roland Kuhn <rk@rkuhn.info>
2024-05-29 20:28:17 +0200

    temperature hysteresis

--- fan-control/fan-control.ino
+++ fan-control/fan-control.ino
@@ -1,34 +1,44 @@
 #include <Arduino_APDS9960.h>
-#include <Arduino_HS300x.h>
+#include <Wire.h>
 
-void red()
-{
-  analogWrite(LEDR, 220);
-  analogWrite(LEDG, 255);
-  analogWrite(LEDB, 255);
-}
+#define SERIAL 1
 
-void green()
-{
-  analogWrite(LEDR, 255);
-  analogWrite(LEDG, 255);
-  analogWrite(LEDB, 220);
-}
+#define FAN_DRIVER D12
+#define TEMP_HIGH 30.0
+#define TEMP_LOW 29.0
 
-void yellow()
-{
-  analogWrite(LEDR, 230);
-  analogWrite(LEDG, 255);
-  analogWrite(LEDB, 230);
-}
+/**
+ * NOTE: On my particular board LEDB and LEDG are swapped.
+ */
 
-void flash()
+#define COLOR(name, red, green, blue)   \
+  void name()                           \
+  {                                     \
+    analogWrite(LEDR, 255 - red / 5);   \
+    analogWrite(LEDB, 255 - green / 5); \
+    analogWrite(LEDG, 255 - blue / 5);  \
+  }
+
+COLOR(red, 255, 0, 0)
+COLOR(green, 0, 255, 0)
+COLOR(blue, 0, 0, 255)
+COLOR(yellow, 255, 255, 0)
+COLOR(purple, 160, 32, 240)
+
+typedef void (*color_t)();
+static color_t colors[] = {red, green, yellow, blue, purple};
+static const int num_colors = sizeof(colors) / sizeof(colors[0]);
+
+enum Mode
 {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
-}
+  OFF = 0,
+  ON = 1,
+  AUTO = 2,
+  AUTO_ON = 3,
+};
+
+float humidity = 0;
+float temperature = 0;
 
 void setup()
 {
@@ -36,14 +46,19 @@ void setup()
   pinMode(LEDG, OUTPUT);
   pinMode(LEDB, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(FAN_DRIVER, OUTPUT);
 
   digitalWrite(LEDR, HIGH);
   digitalWrite(LEDG, HIGH);
   digitalWrite(LEDB, HIGH);
 
+#if SERIAL
   Serial.begin(9600);
+  while (!Serial)
+    ;
+#endif
 
-  if (!APDS.begin() || !HS300x.begin())
+  if (!APDS.begin())
   {
     digitalWrite(LED_BUILTIN, HIGH);
     while (true)
@@ -51,42 +66,120 @@ void setup()
       delay(100);
     }
   }
-}
 
-typedef void (*color_t)();
-static color_t colors[] = {red, green, yellow};
-static const int num_colors = sizeof(colors) / sizeof(colors[0]);
+  Wire1.begin();
+}
 
 void loop()
 {
-  static byte color = 0;
+  static Mode mode = OFF;
+  static byte first = 1;
+  if (first)
+  {
+    colors[mode]();
+    first = 0;
+  }
+
   if (APDS.gestureAvailable())
   {
     int gesture = APDS.readGesture();
+#if SERIAL
+    Serial.print("Gesture: ");
     Serial.println(gesture);
-    for (int i = 0; i <= gesture; i++)
-    {
-      flash();
-    }
+#endif
     if (gesture == GESTURE_UP)
     {
-      color = (color + 1) % num_colors;
-      colors[color]();
+      mode = OFF;
     }
     else if (gesture == GESTURE_DOWN)
     {
-      color = (color + num_colors - 1) % num_colors;
-      colors[color]();
+      mode = ON;
     }
+    else
+    {
+      mode = AUTO;
+    }
+    colors[mode]();
   }
 
-  static float temp = 0;
-  float new_temp = HS300x.readTemperature(CELSIUS);
-  if (new_temp != temp)
+  static unsigned long last_hs3003 = 0;
+  static bool hs3003_read = false;
+
+  unsigned long now = millis();
+  if (now - last_hs3003 > 1000)
   {
-    temp = new_temp;
-    Serial.print("Temperature: ");
-    Serial.print(temp);
-    Serial.println("°C");
+    last_hs3003 = now;
+    wakeHS3003();
+    hs3003_read = true;
   }
+  else if ((now - last_hs3003 > 40) && hs3003_read)
+  {
+    readHS3003();
+    hs3003_read = false;
+  }
+
+  static bool fan = false;
+  if (temperature > TEMP_HIGH && !fan)
+  {
+    digitalWrite(FAN_DRIVER, LOW);
+    fan = true;
+  }
+  else if (temperature < TEMP_LOW && fan)
+  {
+    digitalWrite(FAN_DRIVER, HIGH);
+    fan = false;
+  }
+  if (mode == AUTO && fan)
+  {
+    mode = AUTO_ON;
+    colors[mode]();
+  }
+  else if (mode == AUTO_ON && !fan)
+  {
+    mode = AUTO;
+    colors[mode]();
+  }
+}
+
+void wakeHS3003()
+{
+  Wire1.beginTransmission(0x44);
+  Wire1.write(0x00);
+  Wire1.endTransmission();
+}
+
+void readHS3003()
+{
+  Wire1.requestFrom(0x44, 4);
+  if (!Wire1.available() == 4)
+  {
+#if SERIAL
+    Serial.println("Error: HS3003 did not return 4 bytes");
+#endif
+    return;
+  }
+  uint16_t hum = (Wire1.read() << 8) | Wire1.read();
+  uint16_t temp = (Wire1.read() << 8) | Wire1.read();
+
+  if (hum & 0xc000)
+  {
+#if SERIAL
+    Serial.println("Error: HS3003 returned stale data");
+#endif
+    return;
+  }
+
+  hum &= 0x3FFF;
+  temp >>= 2;
+
+  humidity = ((float)hum / 16383) * 100;
+  temperature = ((float)temp / 16383) * 165 - 40;
+
+#if SERIAL
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.print("%, Temperature: ");
+  Serial.print(temperature);
+  Serial.println("C");
+#endif
 }
